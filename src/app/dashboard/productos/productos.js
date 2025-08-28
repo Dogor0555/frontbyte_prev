4e7ef66c0bda72e3dfc24f537089091787463265
Christopher Briyant Álvarez Moz <89051776+Fort503@users.noreply.github.com>
2025-08-28 14:29:50 -0600

    fix(productos): agregar restricción mínima al campo de stock

--- src/app/dashboard/productos/productos.js
+++ src/app/dashboard/productos/productos.js
@@ -877,6 +877,7 @@ export default function Productos({ initialProductos = [], user }) {
                                         onChange={handleStockChange}
                                         className="text-black w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
                                         required
+                                        min="0"
                                     />
                                 </div>
 
