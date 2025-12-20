d76cc8eebb5c2ecb5fa0857488eeba0c548d0eb1
Fort503 <christopheralvarez503@gmail.com>
2025-12-20 10:19:59 -0600

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = typeof window === "undefined" ? process.env.INTERNAL_API_URL: process.env.NEXT_PUBLIC_API_URL;
+export const API_BASE_URL = typeof window === "undefined"
+	? process.env.INTERNAL_API_URL || 'http://localhost:3000'
+	: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
