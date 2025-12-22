0a7eb65ec27b5e146a07f0261380353c89271dd3
Fort503 <christopheralvarez503@gmail.com>
2025-12-22 15:46:28 -0600

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,4 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ??
-  "https://www.bytefusionsv.com/api";
+export const API_BASE_URL =
+  typeof window === "undefined"
+    ? process.env.INTERNAL_API_URL || "http://api:3000"
+    : process.env.NEXT_PUBLIC_API_URL || "https://www.bytefusionsv.com/api";
